23d88b689c055ed581a08a80219f51e197e6212e
Rosario Scilipoti <rosariosca12@icloud.com>
2023-12-02 12:32:10 +0100

    PROGETTO-FINITO

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+  isActive(route: string): boolean {
+    return this.router.isActive(route, true);
   }
 
+  ngOnInit(): void {}
 }
